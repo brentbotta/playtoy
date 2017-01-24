3f741d721460ca65bde9968cdbf2a53d3abe406b
Brent Botta <brent.botta@gmail.com>
2017-01-23 16:02:28 -0800

    update router file for box and garden pages

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Playtoy' });
 });
 
+/* GET box page. */
+router.get('/box', function(req, res, next) {
+  res.render('box', { title: 'Box' });
+});
+
+/* GET Garden page. */
+router.get('/garden', function(req, res, next) {
+  res.render('garden', { title: 'Garden' });
+});
 module.exports = router;
